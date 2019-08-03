3dfcc56eb271a88803063e0ba914e6241e6e25d2
YadanPhyo <yphyo8739@gmail.com>
2019-08-03 22:36:24 +0630

    css author form

--- src/main/java/demo/example/blogspring/controller/AuthorController.java
+++ src/main/java/demo/example/blogspring/controller/AuthorController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthorController {
@@ -15,13 +18,15 @@ public class AuthorController {
         model.addAttribute("author",new Author());
     return "authorForm";
     }
-    public String process(Author author, BindingResult result){
+    @PostMapping("/author")
+    public String process(@Valid Author author, BindingResult result){
     if (result.hasErrors()){
         return "authorForm";
     }
     authorService.create(author);
     return "redirect:/authors";
     }
+    @GetMapping("/authors")
     public String showallAuthors(Model model){
         model.addAttribute("authors",authorService.findAll());
         return "authors";
--- src/main/java/demo/example/blogspring/model/Author.java
+++ src/main/java/demo/example/blogspring/model/Author.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -14,9 +16,12 @@ public class Author implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+    @Past(message = "Date of birth must be past.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
+    @NotEmpty(message = "Please enter Something!")
     private String interested;
     @Enumerated(EnumType.STRING)
     private Gender gender;
